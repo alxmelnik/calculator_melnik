b4a089e876e11c3d8b8efb79c0fe593b3fa35ee5
Alexsandr <alxmelnik@yandex.ru>
2023-10-11 22:46:06 +0300

    Калькулятор 1,0

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-// let b = "hello";
-// console.log(b);
+let displayValue = document.querySelector(".display__value");
+let buttons = document.querySelectorAll(".btn");
 
+
+// console.log(button.textContent);
+// console.log(displayValue);
+
+buttons.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+
+    if (e.target.textContent === "=") {
+      try {
+        displayValue.textContent = eval(displayValue.textContent);
+      } catch (error) {
+        displayValue.textContent = "Ошибка";
+      }
+    } else if (
+      displayValue.textContent === "0" &&
+      e.target.textContent !== "."
+    ) {
+      displayValue.textContent = e.target.textContent;
+    } else {
+      displayValue.textContent += e.target.textContent;
+    }
+
+    if (e.target.textContent === "AC") {
+      displayValue.textContent = "0";
+    }
+  });
+});
